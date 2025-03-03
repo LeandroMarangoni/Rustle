5e4f74303c64a78165123007337801a2a7d2de4c
Leandro Makino Marangoni <leandro_marangoni@outlook.com>
2025-03-03 17:19:39 -0300

    Creation of dependencies, transactions, block, blockchain and tests

---
+++ src/block.rs
@@ -0,0 +1,35 @@
+use chrono::Utc;
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+use crate::transaction::Transaction;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub index: u64,
+    pub timestamp: i64,
+    pub previous_hash: String,
+    pub hash: String,
+    pub transactions: Vec<Transaction>,
+}
+
+impl Block {
+    pub fn new(index: u64, previous_hash: String, transactions: Vec<Transaction>) -> Self {
+        let timestamp = Utc::now().timestamp();
+        let hash = Block::calculate_hash(index, timestamp, &previous_hash, &transactions);
+
+        Block {
+            index,
+            timestamp,
+            previous_hash,
+            hash,
+            transactions,
+        }
+    }
+
+    pub fn calculate_hash(index: u64, timestamp: i64, previous_hash: &str, transactions: &Vec<Transaction>) -> String {
+        let input = format!("{}{}{}{:?}", index, timestamp, previous_hash, transactions);
+        let mut hasher = Sha256::new();
+        hasher.update(input);
+        format!("{:x}", hasher.finalize())
+    }
+}
---
+++ src/blockchain.rs
@@ -0,0 +1,52 @@
+use crate::block::Block;
+use crate::transaction::Transaction;
+
+pub struct Blockchain {
+    pub chain: Vec<Block>,
+}
+
+impl Blockchain {
+    pub fn new() -> Self{
+        let genesis_transactions = vec![Transaction::new(
+            "System".to_string(),
+            "Genesis".to_string(),
+            0.0,
+        )];
+
+        let genesis_block = Block::new(0, "0".to_string(), genesis_transactions);
+
+        Blockchain {
+            chain: vec![genesis_block],
+        }
+    }
+
+    pub fn add_block(&mut self, transactions: Vec<Transaction>) {
+        let previous_block = self.chain.last().unwrap();
+        let new_block = Block::new(previous_block.index + 1, previous_block.hash.clone(), transactions);
+        self.chain.push(new_block);
+    }
+
+    pub fn is_valid(&self) -> bool {
+        for i in 1..self.chain.len() {
+            let current = &self.chain[i];
+            let previous = &self.chain[i - 1];
+
+            if current.previous_hash != previous.hash {
+                return false;
+            }
+
+            let recalculated_hash = Block::calculate_hash(
+                current.index,
+                current.timestamp,
+                &current.previous_hash,
+                &current.transactions
+            );
+
+            if current.hash != recalculated_hash {
+                return false;
+            }
+        }
+        true
+    }
+
+}
